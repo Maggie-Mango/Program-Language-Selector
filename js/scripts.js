86032fdb56a3e34a422c77101e9f948cccb221db
Maggie S <Maggiesaldivia@gmail.com>
2021-01-30 11:09:58 -0800

    will show that question 3 is working.

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,12 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    let qOneInput = $("input#Q1").val();
+    let qOneInput = $("input#q1").val();
+    let qTwoInput = $("input#q2").val();
+    let qThreeInput = $("#q3").val();
+    let qFourInput = $("input:radio[name=q4]:checked").val();
+
     
-    alert(qOneInput);
+    alert(qThreeInput);
   });
 });
