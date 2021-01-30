0411b2ea1289e187f2692a1d1d39091e243e8a74
Maggie S <Maggiesaldivia@gmail.com>
2021-01-29 16:41:58 -0800

    fix html spacing, add radio button questions and a new card.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,5 @@
 
 
 $(document).ready(function() {
-  $("#formOne").change(function() {
-    event.preventDefault();
-    $("#formTwo").fadeIn();
-  });
-});
 
-$(function(){   
-  jQuery('#input_1_2 input:radio').change(function() {
-      jQuery('#gform_next_button_1_4').trigger('click');
-  });
-});
+
