3936d080c0cb036c378d0039a3e9334e937afe58
Maggie S <Maggiesaldivia@gmail.com>
2021-01-30 14:49:41 -0800

    finish basic branching of the survey.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 
-
-
-// User interface
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
@@ -13,16 +10,49 @@ $(document).ready(function(){
     let qSixInput = $("#color").val();
     
   
-
-    if (qFourInput === 'unsure') {
-      $("h4.header").show()
-        $(".language").text("Ruby")
+    //branching
+    
+    if (qFiveInput === 'mintChoco') {
+      $("h4.header").show();
+      $(".language").text("Rust");
       $(".name").text(qOneInput);
       $(".date").text(qThreeInput);
-      $("#intro").show()       
-      $("#ruby").show()
-    }
+      $(".email").text(qTwoOutput);
+      $("#intro").show();       
+      $("#rust").show();
+    } else if (qFourInput === 'machineLearning') {
+      $("h4.header").show();
+      $(".language").text("Python");
+      $(".name").text(qOneInput);
+      $(".date").text(qThreeInput);
+      $("#intro").show();     
+      $("#pythonData").show();
+    }  else if (qFourInput === 'designingWebsites') {
+      $("h4.header").show();
+      $(".language").text("JavaScript");
+      $(".name").text(qOneInput);
+      $(".date").text(qThreeInput);
+      $("#intro").show();      
+      $("#js").show();
   
+    }  else if (qFourInput === 'infrastructure') {
+      $("h4.header").show();
+      $(".language").text("Python");
+      $(".name").text(qOneInput);
+      $(".date").text(qThreeInput);
+      $("#intro").show();       
+      $("#pythonInfra").show();
+
+    }  else if (qFourInput === 'unsure') {
+      $("h4.header").show();
+      $(".language").text("Ruby");
+      $(".name").text(qOneInput);
+      $(".date").text(qThreeInput);
+      $("#intro").show();       
+      $("#ruby").show();
+
+    }
+    
 
 
   });
